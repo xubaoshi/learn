1c9d965ee87e1ac17fa93eaa0ececca01849c43b
xubaoshi <xubaoshi@xubaoshi.com>
2016-02-01 19:35:47 +0800

    add new

---
+++ project/css/cafe/gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by xu on 2016/1/31.
+ */
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return sass('src/scss/*.scss',{sourcemap:true})
+        .on('error', sass.logError)
+        // For inline sourcemaps
+        .pipe(sourcemaps.write())
+
+        // For file sourcemaps
+        .pipe(sourcemaps.write('maps', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/scss/*scss', ['sass']);
+});
