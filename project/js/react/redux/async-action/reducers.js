55198df28ae339d5ccead83e0ea9c84dab7c84e4
xubaoshi <948869516@qq.com>
2016-09-14 16:12:41 +0800

    add async redux code

---
+++ project/js/react/redux/async-action/reducers.js
@@ -0,0 +1,61 @@
+import {combineReducers} from 'react'
+import {
+    SELECT_SUBREDDIT, INVALIDATE_SUBREDDIT, REQUEST_POSTS, RECEIVE_POSTS
+} from './actions'
+
+function selectedSubreddit(state = 'reactjs', action) {
+    switch (action.type) {
+        case SELECT_SUBREDDIT:
+            return action.subreddit
+        default:
+            return state
+    }
+}
+
+function posts(state = {
+    isFetching: false,
+    didInvalidate: false,
+    items: []
+}, action) {
+    switch (action.type) {
+        case INVALIDATE_SUBREDDIT:
+            return Object.assign({}, state, {
+                didInvalidate: true
+            })
+        case REQUEST_POSTS:
+            return Object.assign({}, state, {
+                isFetching: true,
+                didInvalidate: false
+            })
+        case RECEIVE_POSTS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                didInvalidate: false,
+                items: action.posts,
+                lastUpdated: action.receviedAt
+            })
+        default:
+            return state
+    }
+}
+
+function postsBySubreddit(state = {}, action) {
+    switch (action.type) {
+        case INVALIDATE_SUBREDDIT:
+        case REQUEST_POSTS:
+        case RECEIVE_POSTS:
+            return Object.assign({}, state, {
+                [action.subreddit]: posts(state[action.subreddit], action)
+            })
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({
+    postsBySubreddit,
+    selectedSubreddit
+})
+
+export default rootReducer
+
