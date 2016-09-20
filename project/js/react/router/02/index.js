c0e0a21cd8df6013cc7685c4c0f2e1814ffd4324
xubaoshi <xubaoshi@xubaoshi.com>
2016-09-20 20:49:20 +0800

    add react-router code

--- project/js/react/router/02/index.js
+++ project/js/react/router/02/index.js
@@ -6,8 +6,9 @@ import About from './modules/About'
 import Repos from './modules/Repos'
 render((
     <Router history={hashHistory}>
-        <Route path="/" component={App}/>
-        <Route path="/repos" component={Repos} />
-        <Route path="/about" component={About} />
+        <Route path="/" component={App}>
+            <Route path="/repos" component={Repos} />
+            <Route path="/about" component={About} />
+        </Route>
     </Router>
-), document.getElementById('app'))
+), document.getElementById('app')) 
