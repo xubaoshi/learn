de4bbba4e2bafed020e49ebc193ceecf31a43817
xubaoshi <948869516@qq.com>
2016-09-21 20:17:33 +0800

    add react-router code

--- project/js/react/router/04/modules/App.js
+++ project/js/react/router/04/modules/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import NavLink from './NavLink'
 import Home from './Home'
+import {Link} from 'react-router'
+// import {IndexLink} from 'react-router'
+// <li><IndexLink to="/" activeClassName="active">home</IndexLink></li>
 
 export default React.createClass({
     render() {
@@ -8,6 +11,7 @@ export default React.createClass({
             <div>
                 <h1>Router</h1>
                 <ul>
+                    <li><Link to="/" activeClassName="active" onlyActiveOnIndex={true}> home</Link></li>
                     <li><NavLink to="/about">about</NavLink></li>
                     <li><NavLink to="/repos">repos</NavLink></li>
                 </ul>
