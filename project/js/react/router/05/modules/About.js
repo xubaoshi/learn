b390a058be5f5f5bbc10cdbe91d3828811270c0f
xubaoshi <948869516@qq.com>
2016-09-22 14:52:05 +0800

    add react-router learn code

---
+++ project/js/react/router/05/modules/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+    render(){
+        return (
+            <div>
+                <h2>
+                    This is About
+                </h2>
+            </div>
+        )
+    }
+})
