4db6b43f6ca635bcde30092024b29d4093cc261f
xubaoshi <948869516@qq.com>
2016-10-19 19:43:46 +0800

    edit readme.md

--- project/js/basic/async/custom.js
+++ project/js/basic/async/custom.js
@@ -1,5 +1,9 @@
 var timeout = 0;
-setTimeout(function() {
+// setTimeout(function() {
+//     console.log('1')
+// }, timeout);
+setInterval(function() {
     console.log('1')
 }, timeout);
+console.log('2');
 
