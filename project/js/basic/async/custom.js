abc2b4d21077dd46875a8464e0250761a853b163
xubaoshi <xubaoshi@xubaoshi.com>
2016-10-20 23:39:25 +0800

    edit

--- project/js/basic/async/custom.js
+++ project/js/basic/async/custom.js
@@ -1,9 +1,26 @@
 var timeout = 0;
+
+// 1
+// =======================================
+
 // setTimeout(function() {
 //     console.log('1')
-// }, timeout);
-setInterval(function() {
-    console.log('1')
-}, timeout);
+// }, timeout)
+// setInterval(function() {
+//     console.log('1')
+//}, timeout);
+
+// 2
+// =======================================
+var t = true;
+setTimeout(function(){
+    console.log('1');
+},timeout)
+
+console.log('死循环开始！');
+while(t){}
+
 console.log('2');
+console.log('3');
+console.log('4');
 
