6dde567d18537b9e7879d267397acd8fbee094e2
xubaoshi <948869516@qq.com>
2017-06-19 19:07:22 +0800

    add code

---
+++ project/js/node/nextjs/hello-next/.next/bundles/pages/about.js
@@ -0,0 +1,78 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports =
+webpackJsonp([6],{
+
+/***/ 542:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(__resourceQuery) {
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(12);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "E:\\code\\own\\learn\\project\\js\\node\\nextjs\\hello-next\\pages\\about.js?entry";
+
+exports.default = function () {
+    return _react2.default.createElement("div", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 2
+        }
+    }, _react2.default.createElement("p", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 3
+        }
+    }, "this is about!"));
+};
+
+ ;(function register() { /* react-hot-loader/webpack */ if (true) { if (typeof __REACT_HOT_LOADER__ === 'undefined') { return; } if (typeof module.exports === 'function') { __REACT_HOT_LOADER__.register(module.exports, 'module.exports', "E:\\code\\own\\learn\\project\\js\\node\\nextjs\\hello-next\\pages\\about.js"); return; } for (var key in module.exports) { if (!Object.prototype.hasOwnProperty.call(module.exports, key)) { continue; } var namedExport = void 0; try { namedExport = module.exports[key]; } catch (err) { continue; } __REACT_HOT_LOADER__.register(namedExport, key, "E:\\code\\own\\learn\\project\\js\\node\\nextjs\\hello-next\\pages\\about.js"); } } })();
+    (function (Component, route) {
+      if (false) return
+      if (false) return
+
+      var qs = __webpack_require__(85)
+      var params = qs.parse(__resourceQuery.slice(1))
+      if (params.entry == null) return
+
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(module.exports.default || module.exports, "/about")
+  
+/* WEBPACK VAR INJECTION */}.call(exports, "?entry"))
+
+/***/ }),
+
+/***/ 543:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(542);
+
+
+/***/ })
+
+},[543]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlc1xccGFnZXNcXGFib3V0LmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcGFnZXMvYWJvdXQuanM/NGUxMWE4NSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAoKSA9PiAoXHJcbiAgICA8ZGl2PlxyXG4gICAgICAgIDxwPnRoaXMgaXMgYWJvdXQhPC9wPlxyXG4gICAgPC9kaXY+XHJcbilcclxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vcGFnZXMvYWJvdXQuanM/ZW50cnkiXSwibWFwcGluZ3MiOiI7QTs7Ozs7Ozs7Ozs7O0FBQUE7QUFDQTs7Ozs7OztBQURBO0FBQ0E7O0FBQUE7QUFDQTtBQURBO0FBQUE7O0FBQ0E7QUFBQTtBQUFBO0FBQUE7QUFGQTtBQUNBOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0EiLCJzb3VyY2VSb290IjoiIn0=
+            return { page: comp.default }
+          })
+        
---
+++ project/js/node/nextjs/hello-next/.next/dist/pages/about.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "E:\\code\\own\\learn\\project\\js\\node\\nextjs\\hello-next\\pages\\about.js?entry";
+
+exports.default = function () {
+    return _react2.default.createElement("div", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 2
+        }
+    }, _react2.default.createElement("p", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 3
+        }
+    }, "this is about!"));
+};
